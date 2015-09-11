45f6c9298905b7101bc747a5a2b19673fdcd4ea9
Vladimir Andoga <vladimir.andoga@hotovo.org>
2015-09-11 13:08:52 +0200

    Updated parent pom, fixed typos

--- src/test/java/org/mule/templates/transformers/OpportunityMergeAggregationStrategyTest.java
+++ src/test/java/org/mule/templates/transformers/OpportunityMergeAggregationStrategyTest.java
@@ -12,8 +12,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import junit.framework.Assert;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
